f67e33d5f462045ea429edebeed3690b2e0fff6f
Lin <linmaweb@gmail.com>
2018-02-07 11:15:28 +1000

    initial commit

---
+++ src/SidebarPane.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ChannelList from './ChannelList';
+import PeopleList from './PeopleList';
+
+const SidebarPane = ({
+  channels,
+  people,
+  selectedChannelId,
+  selectedPersonId,
+  onChannelSelected,
+  onPersonSelected
+}) => (
+  <div className='sidebar-pane'>
+    <h1 className="App-title">Sample Reddit with Static Data</h1>
+    <ChannelList
+      channels={channels}
+      selectedId={selectedChannelId}
+      onChannelSelected={onChannelSelected} />
+    <PeopleList
+      people={people}
+      selectedId={selectedPersonId}
+      onPersonSelected={onPersonSelected} />
+  </div>
+);
+
+SidebarPane.propTypes = {
+  channels: PropTypes.array.isRequired,
+  people: PropTypes.array.isRequired,
+  onChannelSelected: PropTypes.func.isRequired,
+  onPersonSelected: PropTypes.func.isRequired,
+  selectedChannelId: PropTypes.number,
+  selectedPersonId: PropTypes.number
+};
+
+export default SidebarPane;
